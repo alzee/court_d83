03355c73c9623ba9bc62662d1c8cd61b46f611ec
Arc Zee <z@arcz.ee>
2020-08-23 15:57:39 +0800

    nth

--- themes/court/js/homeTitles.js
+++ themes/court/js/homeTitles.js
@@ -1,11 +1,11 @@
 function newBadge() {
-	let date = document.getElementsByClassName('date');
-	let now = new Date;
-	let badge = document.createElement('img');
+	var date = document.getElementsByClassName('date');
+	var now = new Date;
+	var badge = document.createElement('img');
 	badge.src= "themes/court/images/new.gif";
-	for(let i = 0; i < date.length; i++){
-		let then = new Date(date[i].innerText.trim().replace('+0800', ''));
-		let hours = (now - then) / 3600000;
+	for(var i = 0; i < date.length; i++){
+		var then = new Date(date[i].innerText.trim().replace('+0800', ''));
+		var hours = (now - then) / 3600000;
 		if(hours < 48){
 			date[i].parentNode.appendChild(badge.cloneNode(true));
 		}
@@ -13,8 +13,8 @@ function newBadge() {
 }
 
 function boldTitle() {
-	let tags = document.getElementsByClassName('tags');
-	for(let i = 0; i < tags.length; i++){
+	var tags = document.getElementsByClassName('tags');
+	for(var i = 0; i < tags.length; i++){
 		if(tags[i].innerText.indexOf('重要') !== -1){
 			tags[i].previousElementSibling.classList.add('font-weight-bold');
 		}
