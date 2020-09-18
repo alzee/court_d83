31ba69ef92c1b5541a1bbcc9b7c7ffb5f446af16
Al Zee <z@alz.ee>
2020-09-18 14:49:24 +0800

    try to fix slick ie8 error

--- themes/bs3/js/initSlick.js
+++ themes/bs3/js/initSlick.js
@@ -1,3 +1,32 @@
+// https://github.com/kenwheeler/slick/issues/3299#issuecomment-426801855
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function(fun) {
+        "use strict";
+
+        if (this == null)
+            throw new TypeError();
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun != "function")
+            throw new TypeError();
+
+        var res = [];
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++)
+        {
+            if (i in t)
+            {
+                var val = t[i]; // in case fun mutates this
+                if (fun.call(thisp, val, i, t))
+                    res.push(val);
+            }
+        }
+
+        return res;
+    };
+}
+
 $(document).ready(function(){
   $('.slick1').slick({
       autoplay: true,
