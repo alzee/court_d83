ce2a422f27e5e17fd76a1eb98b38050f83da8a40
Al Zee <z@alz.ee>
2020-09-18 17:05:58 +0800

    nth

--- themes/bs3/js/homeTitles.js
+++ themes/bs3/js/homeTitles.js
@@ -5,7 +5,12 @@ function newBadge() {
 	badge.src= "themes/bs3/images/new.gif";
 	for(var i = 0; i < date.length; i++){
         // ie8 support 1999/01/01 but not 1999-01-01
-		var then = new Date(date[i].innerText.trim().replace('-', '/'));
+        // chrome support 2020-09-18T14:35:42 but not 2020/09/18T14:35:42
+        var thenStr = date[i].innerText.trim();
+        thenStr = thenStr.replace('-', '/');
+        thenStr = thenStr.replace('T', ' ');
+        thenStr = thenStr.replace('+8000', '');
+		var then = new Date(thenStr);
 		var hours = (now - then) / 3600000;
 		if(hours < 48){
 			date[i].parentNode.appendChild(badge.cloneNode(true));
