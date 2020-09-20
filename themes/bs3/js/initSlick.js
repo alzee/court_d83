2eef64eb88a040002c87fa8971292eee93320209
Al Zee <z@alz.ee>
2020-09-20 10:01:18 +0800

    slick arrows autohide

--- themes/bs3/js/initSlick.js
+++ themes/bs3/js/initSlick.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
-  $('.slick1').slick({
-      autoplay: true,
-      infinite: true,
-      autoplaySpeed: 4000,
-      dots: true,
-      fade: true,
-      cssEase: 'linear',
-      pauseOnFocus: false,
-  });
+    $('.slick1').slick({
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        fade: true,
+        cssEase: 'linear',
+        pauseOnFocus: false,
+    });
 
-  $('.slick2').slick({
-      autoplay: true,
-      infinite: true,
-      autoplaySpeed: 4000,
-      cssEase: 'linear',
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      pauseOnFocus: false,
-  });
+    $('.slick2').slick({
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 4000,
+        cssEase: 'linear',
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        pauseOnFocus: false,
+    });
+
+    $('.slick-slider').hover(
+        function () {
+            $(this).find(".slick-arrow").attr("style", "z-index:1")
+        },
+        function () {
+            $(this).find(".slick-arrow").attr("style", "z-index:-1")
+        }
+    );
 });
